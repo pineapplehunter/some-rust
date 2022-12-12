4c048d4734e4f093f2c69e0e0531e349ce1f0029
Shogo Takata <peshogo+github.com@gmail.com>
2022-12-12 14:53:05 +0900

    update

--- boot.s
+++ boot.s
@@ -4,11 +4,62 @@
 .section .text.init
 .global _start
 _start:
-    beq tp,zero,TOMAIN
-    j .
-TOMAIN:
-    la sp, __RAM_START
-    addi sp, sp,1024
+    call _set_sp
+    call _clear_bss
 
-    call main
+    call loader_main
+    call ram_main
     j .
+
+.global _set_sp
+_set_sp:
+    la sp, __RAM_START
+    addi sp,sp,1024
+    mv a0,tp
+.L_bump_sp:
+    beqz a0, .L_end_set_sp
+    addi sp,sp,1024
+    addi a0,a0,-1
+    j .L_bump_sp
+.L_end_set_sp:
+    ret
+
+.global _clear_bss
+_clear_bss:
+.L_clear_init_done:
+    la a0,init_done
+    li a1,0
+    sb a1,0(a0)
+    bnez tp, .L_wait_for_init
+.L_init_bss:
+    la a0,__BSS_START
+    la a1,__BSS_END
+    li a2,0
+.L_clear_next:
+    sd a2,0(a0)
+    bge a0,a1,.L_end_init_bss
+    addi a0,a0,8
+    j .L_clear_next
+.L_end_init_bss:
+    la a0,init_done
+    li a1,1
+    sb a1,0(a0)
+    j .L_end_clear_bss
+.L_wait_for_init:
+    li a2,100
+.L_wait_inner:
+    beqz a2, .L_check_init
+    addi a2,a2,-1
+    j .L_wait_inner
+.L_check_init:
+    lb a1,0(a0)
+    bnez a1, .L_end_clear_bss
+    j .L_wait_for_init
+.L_end_clear_bss:
+    li a0,0
+    li a1,0
+    li a2,0
+    ret
+
+.section .bss
+init_done: .byte 0
--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,10 @@ struct UARTInner {
     tx: u8,
     _tx_pad: [u8; 3],
     stat: u8,
-    _stat_pad:[u8;3],
+    _stat_pad: [u8; 3],
     control: u8,
-    _control_pad:[u8;3]
+    _control_pad: [u8; 3],
 }
-
 
 lazy_static! {
     static ref UART0: Mutex<UART> = Mutex::new(UART(unsafe { __UART_START }));
@@ -37,16 +36,22 @@ struct UART(usize);
 
 impl fmt::Write for UART {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        let mut uart = Volatile::new(unsafe { &mut *(self.0 as *mut UARTInner) });
-        for c in s.chars() {
-            while uart.map(|v| &v.stat).read() & 0b1000 != 0 {
-                delay(100);
-            }
-            let c = if c.is_ascii() { c as u8 } else { b'.' };
-            uart.map_mut(|v| &mut v.tx).write(c);
-        }
-        Ok(())
+        _print(self, s)
     }
+}
+
+#[no_mangle]
+#[inline(never)]
+fn _print(uart: &mut UART, s: &str) -> fmt::Result {
+    let mut uart = Volatile::new(unsafe { &mut *(uart.0 as *mut UARTInner) });
+    for c in s.chars() {
+        while uart.map(|v| &v.stat).read() & 0b1000 != 0 {
+            delay(100);
+        }
+        let c = if c.is_ascii() { c as u8 } else { b'.' };
+        uart.map_mut(|v| &mut v.tx).write(c);
+    }
+    Ok(())
 }
 
 fn delay(mut amount: usize) {
@@ -57,12 +62,23 @@ fn delay(mut amount: usize) {
 
 static_assertions::const_assert!(size_of::<UARTInner>() == 16);
 
+static mut LOAD_DONE: bool = false;
+
 #[no_mangle]
-pub extern "C" fn main() {
-    writeln!(UART0.lock(), "Hello!").unwrap();
-    writeln!(UART0.lock(), "I am B4Processor!").unwrap();
-    writeln!(UART0.lock(), "UART = {:16X}", unsafe { __UART_START }).unwrap();
-    writeln!(UART0.lock(), "RAM  = {:16X}", unsafe { __RAM_START }).unwrap();
+pub extern "C" fn loader_main(thread_id: usize) {
+    let mut load_done_val = unsafe { Volatile::new(&mut LOAD_DONE) };
+    load_done_val.write(false);
+    if thread_id == 0 {
+        writeln!(UART0.lock(), "Hello!").unwrap();
+        writeln!(UART0.lock(), "I am B4Processor!").unwrap();
+        writeln!(UART0.lock(), "UART = {:16X}", unsafe { __UART_START }).unwrap();
+        writeln!(UART0.lock(), "RAM  = {:16X}", unsafe { __RAM_START }).unwrap();
+        load_done_val.write(true);
+    } else {
+        while !load_done_val.read() {
+            delay(100);
+        }
+    }
 }
 
 #[panic_handler]
